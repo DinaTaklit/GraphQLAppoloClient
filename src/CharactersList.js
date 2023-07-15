788861089fb77ddca7e5b864379c6b9533ad5df2
Dina Taklit <ez_taklit@esi.dz>
2023-07-15 20:49:50 +0100

    feat: create a custome hook

--- src/CharactersList.js
+++ src/CharactersList.js
@@ -1,18 +1,6 @@
-import { useQuery, gql } from "@apollo/client";
-const GET_CHARACTERS = gql`
-  query {
-    characters {
-      results {
-        id
-        name
-        image
-      }
-    }
-  }
-`;
-
+import { useCharacters } from "./hooks/useCharacters";
 export default function CharactersList() {
-  const { loading, error, data } = useQuery(GET_CHARACTERS);
+  const { loading, error, data } = useCharacters();
   if (error) {
     return <div>Error</div>;
   }

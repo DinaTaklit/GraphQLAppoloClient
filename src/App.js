2ffa188e20a58de788c58fed0148085680e05a4f
Dina Taklit <ez_taklit@esi.dz>
2023-07-15 22:27:19 +0100

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import CharactersList from "./pages/characterList";
+import Character from "./pages/character";
 
 function App() {
   return (
     <div className="App">
-      <CharactersList />
+      <Routes>
+        <Route path="/characters" element={<CharactersList />} />
+        <Route path="/characters/:id" element={<Character />} />
+        <Route path="/" element={<CharactersList />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
